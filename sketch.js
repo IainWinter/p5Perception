2a27db99a220c97bef048229b6495264db18e1aa
Iain Winter <iainwinter1@gmail.com>
2017-08-29 01:38:57 -0400

    Added point

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
-void setup() {
+var points = [];
+var p;
+function setup() {
+  createCanvas(500, 500);
 
+  p = new Perceptron([1, 1]);
+
+  for (var i = 0; i < 100; i++) {
+    points[i] = new Point();
+  }
 }
 
-void draw() {
-  elipse(0, 0, 10, 10);
+function draw() {
+  stroke(0)
+  fill(255);
+
+  for (var i = 0; i < points.length; i++) {
+    points[i].show();
+  }
+}
+
+class Point {
+  constructor() {
+    this.x = random(width - 5);
+    this.y = random(height - 5);
+  }
+
+  show() {
+    ellipse(this.x, this.y, 10, 10);
+  }
+}
+
+class Perceptron {
+  constructor(weights) {
+    this.weights = weights;
+  }
 }
